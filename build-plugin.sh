c8c6389391382b25d0e24464de6be7dd04c713aa
falkTX <falktx@falktx.com>
2020-05-30 01:09:54 +0100

    Start with CI
    Signed-off-by: falkTX <falktx@falktx.com>

--- build-plugin.sh
+++ build-plugin.sh
@@ -26,46 +26,50 @@ source setup/versions.sh
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-pfile="${PAWPAW_ROOT}/plugins/${plugin}.json"
+# TODO: make loop
 
-if [ ! -e "${pfile}" ]; then
-    echo "Requested plugin file '${pfile}' does not exist"
-    exit 2
+if true; then
+    pfile="${PAWPAW_ROOT}/plugins/${plugin}.json"
+
+    if [ ! -e "${pfile}" ]; then
+        echo "Requested plugin file '${pfile}' does not exist"
+        exit 2
+    fi
+
+    name=$(jq -crM .name ${pfile})
+    version=$(jq -crM .version ${pfile})
+    buildtype=$(jq -crM .buildtype ${pfile})
+    dlbaseurl=$(jq -crM .dlbaseurl ${pfile})
+
+    # optional args
+    buildargs=$(echo -e $(jq -ecrM .buildargs ${pfile} || echo '\n\n') | tail -n 1)
+    dlext=$(echo -e $(jq -ecrM .dlext ${pfile} || echo '\n\n') | tail -n 1)
+    dlmethod=$(echo -e $(jq -ecrM .dlmethod ${pfile} || echo '\n\n') | tail -n 1)
+
+    download "${name}" "${version}" "${dlbaseurl}" "${dlext}" "${dlmethod}"
+
+    # TODO patch_file support?
+
+    case ${buildtype} in
+        "autoconf")
+            build_autoconf "${name}" "${version}" "${buildargs}"
+            ;;
+        "conf")
+            build_conf "${name}" "${version}" "${buildargs}"
+            ;;
+        "cmake")
+            build_cmake "${name}" "${version}" "${buildargs}"
+            ;;
+        "make")
+            build_make "${name}" "${version}" "${buildargs}"
+            ;;
+        "meson")
+            build_meson "${name}" "${version}" "${buildargs}"
+            ;;
+        "waf")
+            build_waf "${name}" "${version}" "${buildargs}"
+            ;;
+    esac
 fi
-
-name=$(jq -crM .name ${pfile})
-version=$(jq -crM .version ${pfile})
-buildtype=$(jq -crM .buildtype ${pfile})
-dlbaseurl=$(jq -crM .dlbaseurl ${pfile})
-
-# optional args
-buildargs=$(echo -e $(jq -ecrM .buildargs ${pfile} || echo '\n\n') | tail -n 1)
-dlext=$(echo -e $(jq -ecrM .dlext ${pfile} || echo '\n\n') | tail -n 1)
-dlmethod=$(echo -e $(jq -ecrM .dlmethod ${pfile} || echo '\n\n') | tail -n 1)
-
-download "${name}" "${version}" "${dlbaseurl}" "${dlext}" "${dlmethod}"
-
-# TODO patch_file support?
-
-case ${buildtype} in
-    "autoconf")
-        build_autoconf "${name}" "${version}" "${buildargs}"
-        ;;
-    "conf")
-        build_conf "${name}" "${version}" "${buildargs}"
-        ;;
-    "cmake")
-        build_cmake "${name}" "${version}" "${buildargs}"
-        ;;
-    "make")
-        build_make "${name}" "${version}" "${buildargs}"
-        ;;
-    "meson")
-        build_meson "${name}" "${version}" "${buildargs}"
-        ;;
-    "waf")
-        build_waf "${name}" "${version}" "${buildargs}"
-        ;;
-esac
 
 # ---------------------------------------------------------------------------------------------------------------------
