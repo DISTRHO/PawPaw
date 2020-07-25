0d6a7bbb6173d9f0bbe3c12a4c9a56cd5daa9ae4
falkTX <falktx@falktx.com>
2020-07-25 18:39:09 +0100

    Fix up

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,9 @@
 set -e
 
 # Special handling for caching deb archives
-sudo cp ${HOME}/PawPawBuilds/debs/*.deb /var/cache/apt/archives/
+if [ -d ${HOME}/PawPawBuilds/debs ]; then
+    sudo cp ${HOME}/PawPawBuilds/debs/*.deb /var/cache/apt/archives/
+fi
 
 # common
 sudo apt-get install -y build-essential curl cmake jq
