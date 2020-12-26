829a07dce6603b18a26932ad095a516031d35be7
falkTX <falktx@falktx.com>
2020-12-26 00:55:40 +0000

    CI: Put plugin build steps outside yml file, for caching

---
+++ .travis/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+PLUGINS_BASE="abgate artyfx caps die-plugins dpf-plugins fomp mda"
+PLUGINS_CROSS="blop"
+PLUGINS_DISTRHO1="distrho-ports-arctican distrho-ports-dexed distrho-ports-drowaudio distrho-ports-klangfalter distrho-ports-luftikus distrho-ports-obxd distrho-ports-pitched-delay"
+PLUGINS_DISTRHO2="distrho-ports-refine distrho-ports-tal-plugins distrho-ports-temper distrho-ports-vex distrho-ports-wolpertinger"
+
+if [ "${TARGET}" = "linux" ]; then
+    PLUGINS="${PLUGINS_BASE} ${PLUGINS_CROSS}"
+elif [ "${TARGET}" = "macos-old" ]; then
+    PLUGINS="${PLUGINS_BASE}"
+elif [ "${TARGET}" = "macos" ] || [ "${TARGET}" = "macos-universal" ]; then
+    PLUGINS="${PLUGINS_BASE} ${PLUGINS_CROSS} ${PLUGINS_DISTRHO1}"
+elif [ "${TARGET}" = "win32" ] || [ "${TARGET}" = "win64" ]; then
+    PLUGINS="${PLUGINS_BASE} ${PLUGINS_CROSS} ${PLUGINS_DISTRHO1}"
+else
+    exit 1
+fi
+
+${TRAVIS_BUILD_DIR}/build-plugins.sh ${TARGET} ${PLUGINS}
+${TRAVIS_BUILD_DIR}/.cleanup.sh ${TARGET}
+${TRAVIS_BUILD_DIR}/pack-plugins.sh ${TARGET} ${PLUGINS}
