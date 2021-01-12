7c201a338683d07204177525d0c354ff710f5ae0
falkTX <falktx@falktx.com>
2021-01-12 14:56:49 +0000

    Update carla backend
    Signed-off-by: falkTX <falktx@falktx.com>

--- setup/versions.sh
+++ setup/versions.sh
@@ -22,7 +22,7 @@ LIBVORBIS_VERSION=1.3.7
 # ---------------------------------------------------------------------------------------------------------------------
 # plugins
 
-CARLA_VERSION=07349db166957f4aeac120b4d7bffd08e1232b08
+CARLA_VERSION=780a81440a53d51caddab43d1e4bc4539af05d97
 KXSTUDIO_LV2_EXTENSIONS_VERSION=fae65fbc173cd2c4367e85917a6ef97280532d88
 LILV_VERSION=0.24.12
 LV2_VERSION=1.18.0
