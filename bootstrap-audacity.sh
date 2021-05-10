25347ab5b639a61358f2e0a0ddd4626a30007e80
falkTX <falktx@falktx.com>
2021-05-10 20:43:48 +0100

    Continue audacity building; Only 1 step needed for win64
    Signed-off-by: falkTX <falktx@falktx.com>

--- bootstrap-audacity.sh
+++ bootstrap-audacity.sh
@@ -32,41 +32,43 @@ source setup/versions.sh
 # ---------------------------------------------------------------------------------------------------------------------
 # wxwidgets
 
-# wxwidgets_args+=" -DwxWidgets_USE_REL_AND_DBG=no"
-# wxwidgets_args+=" -Daudacity_use_pch=no"
-
 # override PawPaw default
 wxwidgets_args="-DBUILD_SHARED_LIBS=ON"
 
-# win32
-wxwidgets_args+=" -DwxUSE_ACCESSIBILITY=YES"
-wxwidgets_args+=" -DwxBUILD_PRECOMP=YES"
-
+# common flags
 wxwidgets_args+=" -DwxBUILD_CXX_STANDARD=14"
+wxwidgets_args+=" -DwxBUILD_MONOLITHIC=ON"
+wxwidgets_args+=" -DwxBUILD_OPTIMISE=ON"
+wxwidgets_args+=" -DwxBUILD_SHARED=ON"
 wxwidgets_args+=" -DwxUSE_EXPAT=builtin"
 wxwidgets_args+=" -DwxUSE_LIBJPEG=builtin"
 wxwidgets_args+=" -DwxUSE_LIBPNG=builtin"
 wxwidgets_args+=" -DwxUSE_LIBTIFF=builtin"
 wxwidgets_args+=" -DwxUSE_REGEX=builtin"
 wxwidgets_args+=" -DwxUSE_ZLIB=builtin"
-# wxwidgets_args+=" -D"
 
-# custom wxwidgets flags
-# wxwidgets_args+=" -DwxBUILD_MONOLITHIC=ON"
-# wxwidgets_args+=" -DwxBUILD_PRECOMP=OFF"
-# wxwidgets_args+=" -DwxBUILD_OPTIMISE=ON"
-wxwidgets_args+=" -DwxBUILD_SHARED=ON"
-# win32 only
-wxwidgets_args+=" -DwxUSE_WINSOCK2=yes"
+# these match upstream cmake setup
+if [ "${MACOS}" -eq 1 ]; then
+    wxwidgets_args+=" -DwxUSE_ACCESSIBILITY=YES"
+    wxwidgets_args+=" -DwxBUILD_PRECOMP=NO"
+elif [ "${WIN32}" -eq 1 ]; then
+    wxwidgets_args+=" -DwxUSE_ACCESSIBILITY=YES"
+    wxwidgets_args+=" -DwxBUILD_PRECOMP=YES"
+else
+    wxwidgets_args+=" -DwxUSE_ACCESSIBILITY=NO"
+    wxwidgets_args+=" -DwxBUILD_PRECOMP=YES"
+fi
 
-# set(wxSOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR})
-# set(wxBINARY_DIR ${CMAKE_BINARY_DIR})
-# set(wxOUTPUT_DIR ${wxBINARY_DIR}/lib)
+# needed for mingw
+if [ "${WIN32}" -eq 1 ]; then
+    wxwidgets_args+=" -DwxUSE_WINSOCK2=yes"
+    win32_target=_WIN32_WINNT_WIN7
+    export EXTRA_CXXFLAGS="-DWINVER=${win32_target} -D_WIN32_WINNT=${win32_target} -D_WIN32_IE=${win32_target}"
+fi
+
+# needed?
 # set(wxVERSION 3.1.3)
 # set(wxCOPYRIGHT "1992-2019 wxWidgets")
-
-win32_target=_WIN32_WINNT_WIN7
-export EXTRA_CXXFLAGS="-DWINVER=${win32_target} -D_WIN32_WINNT=${win32_target} -D_WIN32_IE=${win32_target}"
 
 download wxWidgets "audacity-fixes-3.1.3" "https://github.com/audacity/wxWidgets.git" "" "git"
 build_cmake wxWidgets "audacity-fixes-3.1.3" "${wxwidgets_args}"
--- build-audacity.sh
+++ build-audacity.sh
@@ -28,64 +28,55 @@ source setup/versions.sh
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-# audacity_repo="https://github.com/jackaudio/jack2.git"
-# audacity_prefix="${PAWPAW_PREFIX}-audacity"
-
+# common flags
 audacity_args=""
-audacity_args+=" -DwxWidgets_USE_REL_AND_DBG=no"
-audacity_args+=" -Daudacity_use_pch=no"
+audacity_args+=" -DwxWidgets_EXCLUDE_COMMON_LIBRARIES=YES"
+audacity_args+=" -DwxWidgets_FIND_COMPONENTS=mono" # FIXME needs forcing
+audacity_args+=" -DwxWidgets_USE_REL_AND_DBG=NO"
 
+audacity_args+=" -Daudacity_use_pch=no"
 audacity_args+=" -Daudacity_use_libflac=system"
 audacity_args+=" -Daudacity_use_libogg=system"
 audacity_args+=" -Daudacity_use_libsndfile=system"
 audacity_args+=" -Daudacity_use_libvorbis=system"
 audacity_args+=" -Daudacity_use_wxwidgets=system"
 
-# TODO
+# TODO build these locally
 audacity_args+=" -Daudacity_use_expat=local"
 audacity_args+=" -Daudacity_use_lame=local"
-audacity_args+=" -Daudacity_use_libsoxr=local"
-audacity_args+=" -Daudacity_use_portaudio-v19=local"
-audacity_args+=" -Daudacity_use_sqlite=local"
 audacity_args+=" -Daudacity_use_libid3tag=local"
 audacity_args+=" -Daudacity_use_libmad=local"
 audacity_args+=" -Daudacity_use_libnyquist=local"
+audacity_args+=" -Daudacity_use_libsbsms=local"
+audacity_args+=" -Daudacity_use_libsoxr=local"
 audacity_args+=" -Daudacity_use_libvamp=local"
 audacity_args+=" -Daudacity_use_lv2=local"
+audacity_args+=" -Daudacity_use_portaudio-v19=local"
 audacity_args+=" -Daudacity_use_portmidi=local"
 audacity_args+=" -Daudacity_use_portmixer=local"
 audacity_args+=" -Daudacity_use_portsmf=local"
-audacity_args+=" -Daudacity_use_libsbsms=local"
 audacity_args+=" -Daudacity_use_soundtouch=local"
+audacity_args+=" -Daudacity_use_sqlite=local"
 audacity_args+=" -Daudacity_use_twolame=local"
 
-# optional
-# audacity_args+=" -Daudacity_use_ffmpeg=local"
-
 # FIXME
+audacity_args+=" -Daudacity_use_ffmpeg=loaded"
 audacity_args+=" -Daudacity_use_pa_jack=off"
 
-# audacity_args+=" -DwxWidgets_ROOT_DIR=${PAWPAW_PREFIX}"
+if [ "${WIN32}" -eq 1 ]; then
+    audacity_args+=" -DwxWidgets_FIND_STYLE=win32" # FIXME needs forcing
+    audacity_args+=" -DwxWidgets_ROOT_DIR=${PAWPAW_PREFIX}"
+    audacity_args+=" -DwxWidgets_LIB_DIR=${PAWPAW_PREFIX}/lib/gcc_x64_dll"
+    audacity_args+=" -DwxWidgets_CONFIGURATION=mswu"
+    audacity_args+=" -DWX_ROOT_DIR=${PAWPAW_PREFIX}"
+    win32_target=_WIN32_WINNT_WIN7
+    export EXTRA_CXXFLAGS="-DWINVER=${win32_target} -D_WIN32_WINNT=${win32_target} -D_WIN32_IE=${win32_target}"
+    export EXTRA_LDFLAGS="-lpthread -lz" # FIXME not working!
+fi
 
-# audacity_args+=" -DwxWidgets_FOUND=BOOL:TRUE"
-# audacity_args+=" -DwxWidgets_INCLUDE_DIRS_NO_SYSTEM"
-
-# audacity_args+=" -DwxWidgets_INCLUDE_DIRS=${PAWPAW_PREFIX}/include;${PAWPAW_PREFIX}/lib/gcc_x64_dll/mswu"
-# audacity_args+=" -DwxWidgets_LIBRARIES='winmm comctl32 uuid oleacc uxtheme rpcrt4 shlwapi version wsock32'"
-# audacity_args+=" -DwxWidgets_LIBRARY_DIRS=${PAWPAW_PREFIX}/lib/gcc_x64_dll"
-# audacity_args+=" -Dtoolkit=mswu"
-# audacity_args+=" -D"
-# audacity_args+=" -D"
-
-audacity_args+=" -DwxWidgets_FIND_STYLE=win32" # must be forced in the cmake file
-audacity_args+=" -DwxWidgets_ROOT_DIR=${PAWPAW_PREFIX}"
-audacity_args+=" -DwxWidgets_LIB_DIR=${PAWPAW_PREFIX}/lib/gcc_x64_dll"
-audacity_args+=" -DwxWidgets_CONFIGURATION=mswu"
-audacity_args+=" -DWX_ROOT_DIR=${PAWPAW_PREFIX}"
-
-win32_target=_WIN32_WINNT_WIN7
-export EXTRA_CXXFLAGS="-DWINVER=${win32_target} -D_WIN32_WINNT=${win32_target} -D_WIN32_IE=${win32_target} -std=gnu++14"
-# -DwxUSE_UNICODE_WINDOWS_H=1"
+# TODO
+# 1. linker flags end up with -lLIB_m-NOTFOUND
+# 2. win32 build requires copying std mingw mutex workarounds
 
 download audacity "e93fdd16c50d9d4630bc64595990e2ee0f96bc17" "https://github.com/KXStudio/audacity.git" "" "git"
 build_cmake audacity "e93fdd16c50d9d4630bc64595990e2ee0f96bc17" "${audacity_args}"
