107cfc883ae268cf3026ea7ecd09c72bda5a7faf
falkTX <falktx@falktx.com>
2022-03-12 12:38:54 +0000

    Build the whole thing for cardinal windows targets
    Signed-off-by: falkTX <falktx@falktx.com>

--- bootstrap-cardinal.sh
+++ bootstrap-cardinal.sh
@@ -22,6 +22,14 @@ export PAWPAW_SKIP_SAMPLERATE=1
 
 ./bootstrap-common.sh "${target}"
 
+# on Windows, we just build the whole thing
+if [ "${target}" = "win32" ] || [ "${target}" = "win64" ]; then
+    ./bootstrap-carla.sh "${target}"
+    exit 0
+fi
+
+# otherwise we build a very tiny set of dependencies
+
 # ---------------------------------------------------------------------------------------------------------------------
 # source setup code
 
